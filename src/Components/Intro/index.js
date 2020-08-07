b639cbe0ee726cbc5323eeb5ea3fef39e706f460
patriciadeboer <pdeboer05@gmail.com>
2020-08-06 20:42:24 -0500

    Mobile friendly intro image

--- src/Components/Intro/index.js
+++ src/Components/Intro/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './styles.scss';
+import mobilePic from '../../images/introPic.jpg'
 
 export const Intro = () =>{
 
   return (
+    <>
     <div className="intro">
-      <div className="hey">Hey! My name is Patricia de Boer</div>
+      <div className="hey">
+        Hey! My name is Patricia de Boer
+      </div>
+      <img src={mobilePic} alt="background-profile" className="mobile-pic" />
     </div>
+
+  </>
   )
 }
